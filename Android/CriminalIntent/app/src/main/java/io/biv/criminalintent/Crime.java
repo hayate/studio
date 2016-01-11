3a94864ab04e05c1618eb0fcf8721cf2949d8bb7
hayate <andrea@byteset.com>
2016-01-11 14:17:26 +0900

    started CriminalIntent learning "fragments"

---
+++ Android/CriminalIntent/app/src/main/java/io/biv/criminalintent/Crime.java
@@ -0,0 +1,28 @@
+package io.biv.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by andrea on 1/11/16.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
