d3d728682334bc3a99c559161dbf41146acb4c8f
hayate <andrea@byteset.com>
2016-02-02 20:21:02 +0900

    HFAD_chapter_10 learn DrawerLayout

--- Android/HFAD/BitsandPizzas/app/src/main/java/io/biv/bitsandpizzas/MainActivity.java
+++ Android/HFAD/BitsandPizzas/app/src/main/java/io/biv/bitsandpizzas/MainActivity.java
@@ -1,21 +1,134 @@
 package io.biv.bitsandpizzas;
 
 import android.app.Activity;
-// import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ShareActionProvider;
+
+import static android.widget.AdapterView.*;
 
 public class MainActivity extends Activity {
 
     private ShareActionProvider mShareActionProvider;
+    private String[] mTitles;
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private int mCurrentPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.open_drawer, R.string.close_drawer) {
+            @Override
+            public void onDrawerClosed(View v) {
+                super.onDrawerClosed(v);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View v) {
+                super.onDrawerClosed(v);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+
+        mTitles = getResources().getStringArray(R.array.titles);
+        mDrawerList = (ListView)findViewById(R.id.drawer);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                mTitles));
+        mDrawerList.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mCurrentPosition = position;
+                Fragment fragment;
+                switch (position) {
+                    case 1:
+                        fragment = new PizzaFragment();
+                        break;
+                    case 2:
+                        fragment = new PastaFragment();
+                        break;
+                    case 3:
+                        fragment = new StoresFragment();
+                        break;
+                    default:
+                        fragment = new TopFragment();
+                }
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.content_frame, fragment, "visible_fragment");
+                ft.addToBackStack(null);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+                MainActivity.this.setActionBarTitle(position);
+
+                mDrawerLayout.closeDrawer(mDrawerList);
+            }
+        });
+        if (savedInstanceState == null) {
+            mDrawerList.setItemChecked(0, true);
+        }
+        else {
+            mCurrentPosition = savedInstanceState.getInt("position");
+            setActionBarTitle(mCurrentPosition);
+        }
+
+        getFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+            @Override
+            public void onBackStackChanged() {
+                FragmentManager fm = getFragmentManager();
+                Fragment f = fm.findFragmentByTag("visible_fragment");
+                if (f instanceof  TopFragment) {
+                    mCurrentPosition = 0;
+                }
+                if (f instanceof PizzaFragment) {
+                    mCurrentPosition = 1;
+                }
+                if (f instanceof  PastaFragment) {
+                    mCurrentPosition = 2;
+                }
+                if (f instanceof StoresFragment) {
+                    mCurrentPosition = 3;
+                }
+                setActionBarTitle(mCurrentPosition);
+                mDrawerList.setItemChecked(mCurrentPosition, true);
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("position", mCurrentPosition);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_share).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
@@ -30,6 +143,10 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         switch (item.getItemId()) {
             case R.id.action_create_order:
                 Intent intent = new Intent(this, OrderActivity.class);
@@ -40,10 +157,34 @@ public class MainActivity extends Activity {
         }
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
     private void setIntent(String text) {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
         intent.putExtra(Intent.EXTRA_TEXT, text);
         mShareActionProvider.setShareIntent(intent);
+    }
+
+    private void setActionBarTitle(int position) {
+        String title;
+        switch (position) {
+            case 0:
+                title = getResources().getString(R.string.app_name);
+                break;
+            default:
+                title = mTitles[position];
+        }
+        getActionBar().setTitle(title);
     }
 }
